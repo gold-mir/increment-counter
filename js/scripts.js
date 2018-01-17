56173a0e658e20011ff48522a47e3ed147f98f06
Manuela Garcia and Miranda Keith <miranda@>
2018-01-17 09:32:43 -0800

    add handling for edge cases (NaN inputs, negative inputs, etc.)

--- js/scripts.js
+++ js/scripts.js
@@ -1,12 +1,18 @@
 $(document).ready(function(){
-
   $("#number-input").submit(function(event){
     event.preventDefault();
 
     var max = parseInt($("input#input-max").val());
     var increment = parseInt($("input#input-increment").val());
-    for(var number = increment; number < max; number += increment){
-      $("#output ul").append("<li>" + number + "</li>")
+    if (!isNaN(max) && !isNaN(increment)  && max >= 0 && increment >= 0 && increment <= max){
+      $("#output").html("<h2>Output:</h2><ul></ul>");
+      for(var number = increment; number <= max; number += increment){
+        $("#output ul").append("<li>" + number + "</li>")
+      }
+    } else {
+      $("#output").html("<h1>Please input two positive numbers. Increment cannot be larger than maximum.</h1>")
     }
+
+
   });
 });
